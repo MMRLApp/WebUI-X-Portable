71ac25515eecda0ec196437fb2fb92d380dc9109
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-08-15 19:50:14 +0200

    Refactor WXLibrary load and unregister methods
    Replaces generic load and unregister methods with inline reified versions in the companion object, simplifying usage and improving type safety.

--- webui/src/main/kotlin/com/dergoogler/mmrl/webui/interfaces/WXLibrary.kt
+++ webui/src/main/kotlin/com/dergoogler/mmrl/webui/interfaces/WXLibrary.kt
@@ -4,18 +4,15 @@ import com.sun.jna.Library
 import com.sun.jna.Native
 
 interface WXLibrary : Library {
-    fun <T : WXLibrary> load(
-        name: String,
-        interfaceClass: Class<T>,
-        options: Map<String, String> = emptyMap(),
-    ): T = Native.load(name, interfaceClass, options)
-
-    fun <T : WXLibrary> unregister(
-        interfaceClass: Class<T>,
-    ) = Native.unregister(interfaceClass)
-
     companion object {
         fun Library.toWXLibrary(): WXLibrary = this as WXLibrary
         fun WXLibrary.toLibrary(): Library = this as Library
+
+        inline fun <reified T : WXLibrary> load(
+            name: String,
+            options: Map<String, String> = emptyMap(),
+        ): T = Native.load(name, T::class.java, options)
+
+        inline fun <reified T : WXLibrary> unregister() = Native.unregister(T::class.java)
     }
 }
